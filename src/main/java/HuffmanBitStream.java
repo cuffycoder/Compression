6335462d91c967af4f097e448acda9e3133a0334
cuffycoder <anybodywantabeer@yahoo.com>
2020-02-15 20:36:58 -0500

    decode base62 string

--- src/main/java/HuffmanBitStream.java
+++ src/main/java/HuffmanBitStream.java
@@ -1,17 +1,65 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HuffmanBitStream {
 
-    private ArrayList<Integer> blockList;
-    private static final int blockSize = Integer.SIZE;
+    private ArrayList<Long> blockList;
+    private static final int blockSize = Long.SIZE - 1; // don't use the sign bit
     private long totalBitsSet;
     private short bitPosWorkingBlock;
+    private static long[] masks;
+
+    private static char[] BASE_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private static HashMap<Character,Integer> baseAlphabetMap;
+    private static String delimitter = " ";
 
     public HuffmanBitStream() {
-        blockList = new ArrayList<Integer>();
+        blockList = new ArrayList<Long>();
         totalBitsSet = 0;
         bitPosWorkingBlock = 0;
+
+        // initialize this to make decoding faster
+        baseAlphabetMap = new HashMap<Character,Integer>();
+
+        int idx = 0;
+        for( char c : BASE_ALPHABET ) {
+            baseAlphabetMap.put( c, idx );
+            idx++;
+        }
+
+
+        // initialize the masks
+        masks = new long[blockSize];
+        masks[0] = 1L;
+
+        for( int i = 1; i < blockSize; i++ ) {
+            masks[i] = masks[i-1] * 2;
+        }
     }
+
+    public HuffmanBitStream( String base62EncodedString ) {
+
+        // first call the normal constructor
+        this();
+
+        String[] parts = base62EncodedString.split( delimitter );
+
+        if( parts.length > 0 ) {
+
+            // first part is the number of bits
+            long numBits = intFromBase62EncodedString( parts[0] );
+
+            this.totalBitsSet = numBits;
+
+            for( int idx = 1; idx < parts.length; idx++ ) {
+                long block = intFromBase62EncodedString( parts[idx] );
+
+                blockList.add( block );
+            }
+        }
+
+    }
+
 
     private long capacity() {
         return (blockList.size() * blockSize) - totalBitsSet;
@@ -21,20 +69,80 @@ public class HuffmanBitStream {
 
         // if no capacity, add another long
         if (capacity() == 0) {
-            blockList.add(0);
-            bitPosWorkingBlock = blockSize - 1;
+            blockList.add(0L);
+            bitPosWorkingBlock = blockSize - 1; // don't use the sign bit
         }
 
+
         if (bitOn) {
-            Integer lastBlock = blockList.get(blockList.size() - 1);
+            Long lastBlock = blockList.get(blockList.size() - 1);
 
-            lastBlock |= 1 << bitPosWorkingBlock;
-
+            lastBlock += masks[bitPosWorkingBlock];
             blockList.set(blockList.size() - 1, lastBlock);
         }
 
         bitPosWorkingBlock--;
+
         totalBitsSet++;
+    }
+
+    public long numBitsInStream() { return totalBitsSet; }
+
+    public String toBase62String() {
+
+        String result = "";
+
+        // first 32 bits are the number of bits in the stream
+        result += intToBase62( numBitsInStream() );
+
+        for( Long block : this.blockList ) {
+            result += delimitter + intToBase62( block );
+        }
+
+        return result;
+    }
+
+    public void showBlocks() {
+
+        for( long block : blockList )
+            System.out.println( block + "\t" + Long.toBinaryString( block ) );
+    }
+
+    public String intToBase62( long n ) {
+        StringBuilder result = new StringBuilder();
+
+        if( 0 == n ) {
+            result.append( BASE_ALPHABET[0] );
+        }
+
+        long base_len = BASE_ALPHABET.length;
+        long c = n;
+
+        while( c != 0 ) {
+            long rem = Long.remainderUnsigned(c, base_len);
+            c = Long.divideUnsigned( c, base_len );
+            result.append( BASE_ALPHABET[ (int) rem] );
+        }
+
+        return result.reverse().toString();
+    }
+
+    public long intFromBase62EncodedString( String base62EncodedString ) {
+        char[] chars = base62EncodedString.toCharArray();
+
+        int base_len = BASE_ALPHABET.length;
+        int num_chars = chars.length;
+        long result = 0;
+        for( int idx = 0; idx < num_chars; idx++ ) {
+            int power = num_chars - ( idx + 1 );
+
+            long digit_value = baseAlphabetMap.get( chars[idx] );
+            long pos_value = (long) Math.pow( base_len, power );
+
+            result +=  pos_value * digit_value;
+        }
+
+        return result;
     }
 
     public String toString() {
@@ -44,12 +152,12 @@ public class HuffmanBitStream {
 
 
         for (int i = 0; i < blockList.size(); i++) {
-                Integer currBlock = blockList.get(i);
+                Long currBlock = blockList.get(i);
                 int bitPos = blockSize - 1;
 
                 while( bitsLeftToPrint > 0 && bitPos >= 0 ) {
 
-                    if ((currBlock & (1 << bitPos)) != 0)
+                    if ((currBlock & (masks[bitPos])) != 0)
                         result += "1";
                     else
                         result += "0";
--- src/test/java/HuffmanBitStreamTest.java
+++ src/test/java/HuffmanBitStreamTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class HuffmanBitStreamTest {
@@ -8,7 +10,7 @@ public class HuffmanBitStreamTest {
     public void testEmptyStream() {
         // Base case: empty stream
         HuffmanBitStream streamEmpty = new HuffmanBitStream();
-        assertTrue( streamEmpty.toString().equals( "" ) );
+        assertTrue(streamEmpty.toString().equals(""));
     }
 
     @Test
@@ -17,43 +19,43 @@ public class HuffmanBitStreamTest {
         // one below, at, and one above the block size
         HuffmanBitStream streamBlockBoundaries = new HuffmanBitStream();
         for (int i = 0; i < 15; i++) {
-            streamBlockBoundaries.addBit(false );
+            streamBlockBoundaries.addBit(false);
             streamBlockBoundaries.addBit(true);
         }
-        streamBlockBoundaries.addBit( true );
+        streamBlockBoundaries.addBit(true);
         String expectedResult = "0101010101010101010101010101011";
-        assertEquals( expectedResult, streamBlockBoundaries.toString());
-        streamBlockBoundaries.addBit( false );
+        assertEquals(expectedResult, streamBlockBoundaries.toString());
+        streamBlockBoundaries.addBit(false);
         expectedResult += "0";
-        assertEquals(expectedResult, streamBlockBoundaries.toString() );
+        assertEquals(expectedResult, streamBlockBoundaries.toString());
 
-        streamBlockBoundaries.addBit( false );
+        streamBlockBoundaries.addBit(false);
         expectedResult += "0";
-        assertEquals( expectedResult, streamBlockBoundaries.toString() );
+        assertEquals(expectedResult, streamBlockBoundaries.toString());
 
-        streamBlockBoundaries.addBit( true );
+        streamBlockBoundaries.addBit(true);
         expectedResult += "1";
-        assertEquals( expectedResult, streamBlockBoundaries.toString() );
+        assertEquals(expectedResult, streamBlockBoundaries.toString());
 
 
         HuffmanBitStream stream2 = new HuffmanBitStream();
 
-        for( int i = 0; i < 32; i++ )
-            stream2.addBit( false );
+        for (int i = 0; i < 32; i++)
+            stream2.addBit(false);
 
-        assertEquals( "00000000000000000000000000000000", stream2.toString() );
-        stream2.addBit( true );
-        assertEquals( "000000000000000000000000000000001", stream2.toString() );
+        assertEquals("00000000000000000000000000000000", stream2.toString());
+        stream2.addBit(true);
+        assertEquals("000000000000000000000000000000001", stream2.toString());
 
 
         HuffmanBitStream stream3 = new HuffmanBitStream();
 
-        for( int i = 0; i < 32; i++ )
-            stream3.addBit( true );
+        for (int i = 0; i < 32; i++)
+            stream3.addBit(true);
 
-        assertEquals( "11111111111111111111111111111111", stream3.toString() );
-        stream3.addBit( false );
-        assertEquals( "111111111111111111111111111111110", stream3.toString() );
+        assertEquals("11111111111111111111111111111111", stream3.toString());
+        stream3.addBit(false);
+        assertEquals("111111111111111111111111111111110", stream3.toString());
     }
 
     @Test
@@ -64,8 +66,100 @@ public class HuffmanBitStreamTest {
         for (int i = 0; i < 50; i++) {
             stream1.addBit(true);
         }
-        stream1.addBit(false );
+        stream1.addBit(false);
         String expectedResult = "111111111111111111111111111111111111111111111111110";
-        assertTrue(stream1.toString().equals( expectedResult) );
+        assertTrue(stream1.toString().equals(expectedResult));
+
+        System.out.println(stream1.toBase62String());
+    }
+
+    @Test
+    public void testBase62EncodeDecode() {
+
+        ArrayList<String> testCases = new ArrayList<String>();
+
+        testCases.add( "" );
+        testCases.add( "0" );
+        testCases.add( "1" );
+        testCases.add( "00" );
+        testCases.add("001");
+        testCases.add("100");
+        testCases.add("101");
+        testCases.add("11111111111111111111111111111111111111111111111110");
+        testCases.add("111111111111111111111111111111111111111111111111110");
+        testCases.add("1111111111111111111111111111111111111111111111111111111111111111");
+        testCases.add("11111111111111111111111111111111111111111111111111111111111111111");
+        testCases.add("00000000000000000000000000000000");
+        testCases.add("0000000000000000000000000000000000000000000000000000000000000000");
+        testCases.add("00000000000000000000000000000000000000000000000000000000000000000");
+        testCases.add("00000000000000000000000000000000000000000000000000000000000000001");
+        testCases.add("11111111111111111111111111111111111111111111111111111111111111111");
+        testCases.add("111111111111111111111111111111111111111111111111110111111111111111101111111111111111011111111111111110");
+        
+
+        for (String testCase : testCases) {
+            HuffmanBitStream origStream = new HuffmanBitStream();
+
+            for (char c : testCase.toCharArray()) {
+                if ('1' == c) {
+                    origStream.addBit(true);
+                } else
+                    origStream.addBit(false);
+            }
+
+            System.out.println("\n\n");
+            System.out.println("Test String     : " + testCase);
+            System.out.println("Stream As String: " + origStream.toString());
+            System.out.println("Stream As Base62: " + origStream.toBase62String());
+
+            assertTrue(origStream.toString().equals(testCase));
+            assertTrue(origStream.numBitsInStream() == testCase.length());
+
+            HuffmanBitStream reconstructedStream = new HuffmanBitStream(origStream.toBase62String());
+            System.out.println("Reconstructed As String: " + reconstructedStream.toString());
+            System.out.println("Reconstructed As Base62: " + reconstructedStream.toBase62String());
+
+            origStream.showBlocks();
+            reconstructedStream.showBlocks();
+
+            assertTrue(reconstructedStream.numBitsInStream() == origStream.numBitsInStream());
+            assertTrue(reconstructedStream.toString().equals(testCase));
+            assertTrue(reconstructedStream.toString().equals(origStream.toString()));
+            assertTrue(reconstructedStream.toBase62String().equals(origStream.toBase62String()));
+        }
+
+    }
+
+    @Test
+    public void scratchTest() {
+        HuffmanBitStream s = new HuffmanBitStream();
+
+        long[] tests = {0L,
+//                1L,
+//                2L,
+//                200L,
+//                3000L,
+//                4000000L,
+                1152921504606846976L};
+
+        System.out.println( "Max Value: " + Long.MAX_VALUE );
+        System.out.println( "Problem:   " + 1152921504606846976L );
+
+        long increment = 2;
+        int num_iters = 0;
+
+//        for (long l = 0L; l < Long.MAX_VALUE; l+=increment ) {
+        for( long l : tests ) {
+            String encoded = s.intToBase62(l);
+            long decoded = s.intFromBase62EncodedString(encoded);
+
+            if (true || decoded != l) {
+                System.out.println(l);
+                System.out.println(encoded);
+                System.out.println(decoded);
+                System.out.println("\n");
+            }
+
+        }
     }
 }
